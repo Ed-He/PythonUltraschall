262267c6b47ade7382d264120098449c6867da0a
ed-he <eduard.hermus@outlook.com>
2023-02-24 11:40:49 +0100

    cleaned code

--- ultraschall.py
+++ ultraschall.py
@@ -4,50 +4,44 @@ import RPi.GPIO as GPIO
 from paho.mqtt import client as mqtt_client
 from datetime import datetime
 
+
 def main():
-    arraySize = 10  # Die Menge an Distanzen die überprüft werden
+    array_size = 10  # Die Menge an Distanzen die überprüft werden
     tolerance = 1  # Toleranz einstellung wie genau berechnet wird ob die Maschine läuft
-    sleepTime = 0.5 # Wartezeit zwischen Messungen
+    sleep_time = 0.5  # Wartezeit zwischen Messungen
 
     iteration = 0  # initialisierung der ersten Iteration
-    statusRunning = False  # Initialisierung des Maschinenstatus
-    measurements = [0] * arraySize  # Array Größe initialisieren
+    status_running = False  # Initialisierung des Maschinenstatus
+    measurements = [0] * array_size  # Array Größe initialisieren
 
     data = {}
-
     GPIO.setmode(GPIO.BCM)
-    speedSound = 34300
+    speed_sound = 34300
 
-    #MQTT-Konfiguration für Kommunikation mit Node-Red
+    # MQTT-Konfiguration für Kommunikation mit Node-Red
     broker = 'localhost'
     port = 1883
-    #topic = "python/ultrasonic/distance"
+    # topic = "python/ultrasonic/distance"
     client_id = f'python-mqtt-ultrasonic'
 
-    # Definition von Variablen/ Listen
-    publishInterval = 1 #Wartezeit nach jedem Messvorgang
-    #data={
-    #    "status": "" #enthält letzten Status
-    #    }
-
-    def on_connect(client, userdata, flags, rc):
+    def on_connect(rc):
         if rc == 0:
             print("Connected to MQTT Broker!")
         else:
             print("Failed to connect, return code %d\n", rc)
 
-    def on_message(client, userdata, msg):
-        settings=json.loads(msg.payload)
+    def on_message(msg):
+        settings = json.loads(msg.payload)
         print("received: " + str(settings))
-        #setzen von publishInterval
+        # setzen von publishInterval
         nonlocal publishInterval
-        publishInterval=float(settings["publishInterval"])
+        publishInterval = float(settings["publishInterval"])
 
     def get_distance(trigger, echo):
 
         GPIO.output(trigger, False)
 
-        time.sleep(sleepTime)
+        time.sleep(sleep_time)
 
         GPIO.output(trigger, True)
 
@@ -55,7 +49,7 @@ def main():
 
         GPIO.output(trigger, False)
 
-        triggerTime = time.time()
+        trigger_time = time.time()
 
         start = 0
         stop = 0
@@ -63,30 +57,29 @@ def main():
         while GPIO.input(echo) == 0:
             start = time.time()
 
-            if time.time()-triggerTime > 0.1:  # war 0,1, wenn länger als 0.1 Sekunden FALSE --> Messung ungültig
+            if time.time() - trigger_time > 0.1:  # war 0,1, wenn länger als 0.1 Sekunden FALSE --> Messung ungültig
                 return -2
 
-
-        while GPIO.input(echo)==1:
+        while GPIO.input(echo) == 1:
             stop = time.time()
 
-        #berechnet distanz
-        distance = ((stop-start) * speedSound) / 2
+        # berechnet distanz
+        distance = ((stop - start) * speed_sound) / 2
 
-        if(distance > 4000):
+        if (distance > 4000):
             return -1
 
         return distance
 
     def read_sensor(trigger, echo, itteration):
-        GPIO.setup(trigger,GPIO.OUT)  # Trigger
-        GPIO.setup(echo,GPIO.IN)      # Echo
+        GPIO.setup(trigger, GPIO.OUT)  # Trigger
+        GPIO.setup(echo, GPIO.IN)  # Echo
 
         distance = get_distance(trigger, echo)
 
         measurements[itteration] = distance
 
-        time.sleep(sleepTime)
+        time.sleep(sleep_time)
 
     def publishBroker(client, status, topicName):
         data["status"] = status
@@ -109,36 +102,36 @@ def main():
     # client.subscribe("python/ultrasonic/settings", qos=2)  # auf Topic subscriben
 
     try:
-        start = datetime.now()
-        aktiv = datetime.now() - start
-        inaktiv = datetime.now() - start
+        start_time = datetime.now()
+        aktiv_time = datetime.now() - start_time
+        inaktiv_time = datetime.now() - start_time
         while True:
             read_sensor(20, 12, iteration)
 
             iteration = iteration + 1
 
             # Nach einer vollständigen Messung wird die Iteration zurückgesetzt und Daten erfasst
-            if (iteration >= arraySize):
+            if iteration >= array_size:
                 iteration = 0
 
                 # Durchlauf der gemessenen Werte und Erfassen des Maschinenstatus
-                for x in range(arraySize):
-                    if (measurements[x] == -1):
+                for x in range(array_size):
+                    if measurements[x] == -1:
                         print("Distanz ist zu Weit! Größer 4 Meter")
-                        statusRunning = False
-                    elif (measurements[x] == -2):
+                        status_running = False
+                    elif measurements[x] == -2:
                         print("Messung ist ungültig!!")
                         # statusRunning = False
-                    elif (abs(measurements[x] - measurements[0]) >= tolerance):
-                        statusRunning = True
+                    elif abs(measurements[x] - measurements[0]) >= tolerance:
+                        status_running = True
                     else:
-                        statusRunning = False
+                        status_running = False
 
-            if (statusRunning == True):
-                aktiv = datetime.now() - (start + inaktiv)
+            if status_running:
+                aktiv_time = datetime.now() - (start_time + inaktiv_time)
                 msg = "Maschine is Running"
             else:
-                inaktiv = datetime.now() - (start + aktiv)
+                inaktiv_time = datetime.now() - (start_time + aktiv_time)
                 msg = "Maschine is off"
 
             publishBroker(client, msg, "status")
@@ -147,5 +140,6 @@ def main():
     except KeyboardInterrupt:
         GPIO.cleanup()
         print("Programm abgebrochen")
+
 
 main()
